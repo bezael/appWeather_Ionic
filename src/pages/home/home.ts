688e490f16acca57b36e8ca954b1947ea04c1e00
bezael <bezael_e@hotmail.com>
2017-08-15 14:15:30 +0200

    Initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WeatherProvider } from '../../providers/weather/weather';
+
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	weather:any;
 
-  constructor(public navCtrl: NavController) {
+	location:{
+		state:string,
+		city:string
+	}
+
+  constructor(public navCtrl: NavController,
+  	private weatherProvider:WeatherProvider,
+  	private storage:Storage) {
 
   }
 
+  ionViewWillEnter(){
+  	this.storage.get('location').then((val => {
+
+  		if(val != null ){
+  			console.log('55555');
+  			this.location = JSON.parse(val);	
+  		}else{
+  			console.log('888888');
+  			this.location = {
+  				state:'ES',
+  				city:'Barcelona'
+  			}
+  		}
+
+  		this.weatherProvider.getWeather(this.location.state, this.location.city).subscribe( weather => {
+  			this.weather = weather.current_observation;
+  		});
+  	}));  	
+  }
 }
